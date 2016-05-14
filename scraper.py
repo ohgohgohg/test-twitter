86337312d3d6128b7220842bdaf286a2d7e4e207
ohg <ohgohgohg@users.noreply.github.com>
2016-05-14 08:51:11 +0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -22,3 +22,44 @@
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+
+###############################################################################
+# Twitter srcaper for the hashtag #bbcsms.
+# https://github.com/aburan28/scrape10_twitter_scraper_4/blob/master/scraper.py
+###############################################################################
+
+import scraperwiki
+import simplejson
+
+# retrieve a page
+base_url = 'http://search.twitter.com/search.json?q='
+q = 'microphone'
+options = '&rpp=100&page='
+page = 1
+import collections
+cnt = collections.Counter()
+cnt['I'] += 1
+
+while 1:
+    q = cnt.popitem()
+    try:
+        url = base_url + q + options + str(page)
+        html = scraperwiki.scrape(url)
+        #print html
+        soup = simplejson.loads(html)
+        for result in soup['results']:
+            data = {}
+            data['id'] = result['id']
+            data['text'] = result['text']
+            for word in data['text']:
+                cnt[word] += 1 
+                
+            data['from_user'] = result['from_user']
+            # save records to the datastore
+            scraperwiki.datastore.save(["id"], data) 
+        page = page + 1
+    except:
+        print str(page) + ' pages scraped'
+        break
+        
+    
